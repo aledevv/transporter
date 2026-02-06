c7dc228972e8d86e098d7c977a8f251612a95856
aledevv <alessandro.devidi@studenti.unitn.it>
2026-02-06 12:54:58 +0100

    successfully deployed the service

--- gcloud_deploy.sh
+++ gcloud_deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "🚀 Deploy su Cloud Run in corso..."
+echo "🚀 Deploy su Cloud Run (progetto bus-plan-6d002)..."
 gcloud run deploy transporter \
   --source . \
   --platform managed \
   --region europe-west6 \
   --allow-unauthenticated \
+  --project bus-plan-6d002 \
   --clear-base-image
 
 firebase deploy --only hosting:busplan
